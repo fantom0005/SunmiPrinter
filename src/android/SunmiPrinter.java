d7156e8a89bc7096e3f800ae5de6717cb9a080e0
Anton Shvetsov <antonshvecow@gmail.com>
2016-11-24 13:35:31 +0700

    Service connector

--- src/android/SunmiPrinter.java
+++ src/android/SunmiPrinter.java
@@ -3,6 +3,12 @@ package sunmi.printer;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import sunmi.printer.ICallback;
+import sunmi.printer.IWoyouService;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
+import android.os.IBinder;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +17,23 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class SunmiPrinter extends CordovaPlugin {
+
+    private IWoyouService woyouService;
+    private ICallback callback = null;
+
+    private ServiceConnection connService = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+
+			woyouService = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			woyouService = IWoyouService.Stub.asInterface(service);
+		}
+	};
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
